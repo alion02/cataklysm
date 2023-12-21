dc9c16bcf6c08cbd8957aec39549604ec1997952
alion02 <alojczek@gmail.com>
2023-12-21 23:35:44 +0100

    Improve perft output

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,12 @@ fn main() {
             for d in 0.. {
                 let time = Instant::now();
                 let nodes = game.perft(d);
-                let nps = nodes as f64 / time.elapsed().as_secs_f64();
-                println!("depth {d}: {:.1} Mnps, {nodes} nodes", nps / 1_000_000.);
+                let secs = time.elapsed().as_secs_f64();
+                let nps = nodes as f64 / secs;
+                println!(
+                    "depth {d}: {nodes} nodes in {secs:.2}s ({:.1} Mnps)",
+                    nps / 1_000_000.,
+                );
             }
         }
         _ => panic!(),
