ab23ff0e35832080137bd151949fb025db4606ce
alion02 <alojczek@gmail.com>
2023-12-22 20:14:25 +0100

    Make stacks with different capacities available

--- src/stack.rs
+++ src/stack.rs
@@ -35,70 +35,79 @@ impl Iterator for DropCounts {
     }
 }
 
-mod stack64 {
-    use super::*;
+macro_rules! stack {
+    ($mod:ident, $export:ident, $StackBacking:ty) => {
+        pub use $mod::Stack as $export;
+        mod $mod {
+            use super::*;
 
-    type StackBacking = u64;
+            type StackBacking = $StackBacking;
 
-    #[derive(Clone, Copy, PartialEq, Eq)]
-    pub struct Stack(StackBacking);
+            #[derive(Clone, Copy, PartialEq, Eq)]
+            pub struct Stack(StackBacking);
 
-    impl Default for Stack {
-        #[inline(always)]
-        fn default() -> Self {
-            Self::EMPTY
+            impl Default for Stack {
+                #[inline(always)]
+                fn default() -> Self {
+                    Self::EMPTY
+                }
+            }
+
+            impl Stack {
+                pub const EMPTY: Self = Self(1);
+                const CAPACITY: u32 = StackBacking::BITS - 1;
+
+                #[inline(always)]
+                pub fn one_tall(color: bool) -> Self {
+                    Self(0b10 | color as StackBacking)
+                }
+
+                #[inline(always)]
+                pub fn height(self) -> u32 {
+                    self.0.leading_zeros() ^ StackBacking::BITS - 1
+                }
+
+                #[inline(always)]
+                pub fn is_empty(self) -> bool {
+                    self == Stack::EMPTY
+                }
+
+                #[inline(always)]
+                pub fn drop(&mut self, hand: &mut Hand, count: u32) {
+                    debug_assert!(count != 0);
+                    debug_assert!(self.height() + count <= Stack::CAPACITY);
+
+                    self.0 =
+                        self.0 << count | hand.0.wrapping_shr(count.wrapping_neg()) as StackBacking;
+                    hand.0 <<= count;
+                }
+
+                #[inline(always)]
+                pub fn take(&mut self, count: u32) -> Hand {
+                    debug_assert!(count != 0);
+                    debug_assert!(self.height() >= count);
+
+                    let r = Hand((self.0 as u32).wrapping_shl(count.wrapping_neg()));
+                    self.0 >>= count;
+                    r
+                }
+
+                #[inline(always)]
+                pub fn top(self) -> Option<bool> {
+                    (!self.is_empty()).then_some(self.0 & 1 != 0)
+                }
+
+                #[inline(always)]
+                pub fn top_unchecked(self) -> bool {
+                    debug_assert!(!self.is_empty());
+                    self.0 & 1 != 0
+                }
+            }
         }
-    }
-
-    impl Stack {
-        pub const EMPTY: Self = Self(1);
-        const CAPACITY: u32 = StackBacking::BITS - 1;
-
-        #[inline(always)]
-        pub fn one_tall(color: bool) -> Self {
-            Self(0b10 | color as StackBacking)
-        }
-
-        #[inline(always)]
-        pub fn height(self) -> u32 {
-            self.0.leading_zeros() ^ StackBacking::BITS - 1
-        }
-
-        #[inline(always)]
-        pub fn is_empty(self) -> bool {
-            self == Stack::EMPTY
-        }
-
-        #[inline(always)]
-        pub fn drop(&mut self, hand: &mut Hand, count: u32) {
-            debug_assert!(count != 0);
-            debug_assert!(self.height() + count <= Stack::CAPACITY);
-
-            self.0 = self.0 << count | hand.0.wrapping_shr(count.wrapping_neg()) as StackBacking;
-            hand.0 <<= count;
-        }
-
-        #[inline(always)]
-        pub fn take(&mut self, count: u32) -> Hand {
-            debug_assert!(count != 0);
-            debug_assert!(self.height() >= count);
-
-            let r = Hand((self.0 as u32).wrapping_shl(count.wrapping_neg()));
-            self.0 >>= count;
-            r
-        }
-
-        #[inline(always)]
-        pub fn top(self) -> Option<bool> {
-            (!self.is_empty()).then_some(self.0 & 1 != 0)
-        }
-
-        #[inline(always)]
-        pub fn top_unchecked(self) -> bool {
-            debug_assert!(!self.is_empty());
-            self.0 & 1 != 0
-        }
-    }
+    };
 }
 
-pub use stack64::Stack as Stack64;
+stack!(stack16, Stack16, u16);
+stack!(stack32, Stack32, u32);
+stack!(stack64, Stack64, u64);
+stack!(stack128, Stack128, u128);
