7bc724c3af181eef9b2aee20e7227877f63ffe50
alion02 <alojczek@gmail.com>
2024-01-15 03:55:56 +0100

    Show initialization time

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
-use std::{env::args, time::Instant};
+use std::{
+    env::{args, Args},
+    time::Instant,
+};
 
 use cataklysm::game::*;
 
 fn main() {
     let mut args = args();
 
+    let make_game = |mut args: Args| {
+        let time = Instant::now();
+        let tps = args.next().unwrap();
+        let game = new_game(Options::from_position(Position::Tps(&tps)).unwrap()).unwrap();
+        println!(
+            "initialized in {:.1}ms",
+            time.elapsed().as_secs_f64() * 1000.,
+        );
+        game
+    };
+
     match args.nth(1).unwrap().as_str() {
         "perft" => {
-            let tps = args.next().unwrap();
-            let mut game = new_game(Options::from_position(Position::Tps(&tps)).unwrap()).unwrap();
+            let mut game = make_game(args);
             for d in 1.. {
                 for mode in [PerftMode::Batch, PerftMode::Naive] {
                     let time = Instant::now();
@@ -28,8 +41,7 @@ fn main() {
             }
         }
         "search" => {
-            let tps = args.next().unwrap();
-            let mut game = new_game(Options::from_position(Position::Tps(&tps)).unwrap()).unwrap();
+            let mut game = make_game(args);
             for d in 1..30 {
                 let time = Instant::now();
                 let (eval, action) = game.search(d);
