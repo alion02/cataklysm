51a21b0030ff8846b6ca55d69c4ca78d0a4d5a5c
alion02 <alojczek@gmail.com>
2024-02-06 07:49:54 +0100

    Cancel previous commit
    Doesn't handle connecting new groups. Try again later.

--- generic/src/state.rs
+++ generic/src/state.rs
@@ -324,7 +324,7 @@ impl State {
 
                 if piece.is_road() {
                     s.road[color] ^= bit;
-                    s.influence[color].extend(bit);
+                    s.influence[color].compute(s.road[color], false);
                 }
 
                 if piece.is_block() {
