ad5409ba7a9edf09dad3dec9880f45700b4c3eb4
alion02 <alojczek@gmail.com>
2024-03-08 06:56:33 +0100

    Port Hash

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -7,12 +7,14 @@ extern crate alloc;
 
 pub mod color;
 pub mod direction;
+pub mod hash;
 pub mod pair;
 pub mod piece;
 pub mod wrapping_array;
 
 pub use color::{Color::*, *};
 pub use direction::{Direction::*, *};
+pub use hash::*;
 pub use pair::*;
 pub use piece::{Piece::*, *};
 pub use wrapping_array::*;
