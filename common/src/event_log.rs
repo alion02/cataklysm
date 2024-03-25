8a50190fd8877143d0523803fa520dfb24e04b9f
alion02 <alojczek@gmail.com>
2024-03-26 00:37:43 +0100

    Clean up, reorganize

--- common/src/event_log.rs
+++ common/src/event_log.rs
@@ -1,47 +1,66 @@
 #[cfg(feature = "event-log")]
 mod inner {
-    use core::hash::Hash;
-
+    use crate::Event;
+    use alloc::boxed::Box;
     use hashbrown::HashMap;
+    use rand_chacha::ChaCha20Rng;
+    use rand_distr::{Distribution, Geometric};
 
-    pub struct EventLog<E> {
+    pub struct EventLog {
         skip: u32,
-        events: HashMap<E, u64>,
+        inner: Box<EventLogInner>,
     }
 
-    impl<E> EventLog<E> {
+    struct EventLogInner {
+        events: HashMap<Event, u64>,
+        dist: Geometric,
+        rng: ChaCha20Rng,
+    }
+
+    impl EventLog {
         #[inline]
         pub fn should_log(&mut self) -> bool {
             self.skip -= 1;
             self.skip == 0
         }
-    }
 
-    impl<E: Hash + Eq> EventLog<E> {
         #[cold]
         #[inline(never)]
-        pub fn log(&mut self, event: E) {
-            *self.events.entry(event).or_default() += 1;
+        pub fn log(&mut self, event: Event) {
+            *self.inner.events.entry(event).or_default() += 1;
+            self.skip = self.inner.dist.sample(&mut self.inner.rng) as u32 + 1;
         }
     }
 }
 
 #[cfg(not(feature = "event-log"))]
 mod inner {
-    use core::marker::PhantomData;
+    use crate::Event;
 
-    pub struct EventLog<E>(PhantomData<E>);
+    pub struct EventLog;
 
-    impl<E> EventLog<E> {
+    impl EventLog {
         #[inline]
         pub fn should_log(&mut self) -> bool {
             false
         }
 
-        #[cold]
-        #[inline(never)]
-        pub fn log(&mut self, event: E) {}
+        pub fn log(&mut self, _: Event) {}
     }
 }
 
 pub use inner::*;
+
+#[derive(Hash, PartialEq, Eq)]
+pub struct Event {
+    pub ply: u16,
+    pub kind: EventKind,
+}
+
+#[derive(Hash, PartialEq, Eq)]
+pub enum EventKind {
+    MakePlaceFlat,
+    MakePlaceWall,
+    MakePlaceCap,
+    PlacementExpansionIterations(u8),
+}
--- generic/src/lib.rs
+++ generic/src/lib.rs
@@ -11,9 +11,9 @@
 
 extern crate alloc;
 
-mod game;
 mod influence;
 mod params;
+mod rules;
 mod tt;
 mod util;
 
@@ -34,7 +34,6 @@ use influence::*;
 use params::*;
 use tt::*;
 use util::*;
-use EventKind::*;
 
 const HAND: u32 = SIZE as u32;
 
@@ -83,11 +82,11 @@ struct UpdateState {
     caps_left: Pair<u8>,
     generation: u8,
     half_komi: i8,
+    log: EventLog,
     stacks: [Stack; ROW_LEN],
     inactive_abort: Arc<AtomicBool>,
     hashes: WrappingArray<u64, 64>,
     killers: WrappingArray<u16, 32>,
-    log: EventLog<Event>,
 }
 
 #[repr(C)]
@@ -121,20 +120,7 @@ struct UnmakePlace {
 #[derive(Clone, Copy)]
 struct UnmakeSpread {}
 
-#[derive(Hash, PartialEq, Eq)]
-struct Event {
-    ply: u16,
-    kind: EventKind,
-}
-
-#[derive(Hash, PartialEq, Eq)]
-enum EventKind {
-    MakePlaceFlat,
-    MakePlaceWall,
-    MakePlaceCap,
-    PlacementExpansionIterations(u8),
-}
-
+#[macro_export]
 macro_rules! log {
     ($s:ident, $c:expr, $e:expr) => {
         if $s.update.log.should_log() {
@@ -169,145 +155,5 @@ impl<'a> State<'a> {
     #[inline]
     fn piece(&self) -> Bb {
         self.copy.road | self.copy.noble
-    }
-
-    #[inline]
-    fn for_actions<B, C>(
-        &mut self,
-        mut acc: C,
-        f: fn(State, C, u16) -> ControlFlow<B, C>,
-    ) -> ControlFlow<B, C> {
-        let empty = self.piece() ^ BOARD;
-        macro_rules! for_placements {
-            ($pc:expr) => {
-                let mut left = empty;
-                // Assume actions are not generated on completed games.
-                loop {
-                    acc = f(self.lend(), acc, left.trailing_zeros() as u16 | $pc << 6)?;
-                    left &= left - 1;
-                    if left == 0 {
-                        break;
-                    }
-                }
-            };
-        }
-
-        // Only do stone placements if we have any stones in reserve. We're technically checking
-        // the wrong reserves on the first move because we assume this passes anyway.
-        if self.update.stones_left[self.player()] > 0 {
-            for_placements!(1);
-
-            // If it's the first move, skip over all the other kinds of actions. This check gets
-            // skipped if we don't have stones, but we again don't care.
-            if self.copy.ply < 2 {
-                return Continue(acc);
-            }
-
-            // Wall placements.
-            for_placements!(2);
-        }
-
-        if self.update.caps_left[self.player()] > 0 {
-            // Cap placements.
-            for_placements!(3);
-        }
-
-        Continue(acc)
-    }
-
-    #[no_mangle]
-    #[inline]
-    fn make(&mut self, new: &mut Out<CopyState>, unmake: &mut Out<Unmake>, action: u16) {
-        let new_ply = self.copy.ply + 1;
-        new.val.ply = new_ply;
-
-        let mut hash = self.update.hashes[self.copy.ply] ^ HASH_SIDE_TO_MOVE;
-
-        let player = self.player();
-        let pat = action >> 8;
-        let sq = action & 0x3f;
-        if pat == 0 {
-            hash ^= HASH_PC_SQ[action as usize].load(Relaxed);
-            hash ^= HASH_STACK[0].load(Relaxed); // TODO
-
-            log!(self, match action >> 6 {
-                1 => MakePlaceFlat,
-                2 => MakePlaceWall,
-                3 => MakePlaceCap,
-                _ => unreachable!(),
-            });
-
-            // Check if it's the first move (ply 0 or 1).
-            let player = player ^ (self.copy.ply < 2); // Strange codegen. AND with 1 thrice.
-            let inf = &mut self.update.influence.pair_mut()[player];
-
-            // Unconditionally store. Not worth checking if it needs unmaking.
-            unmake.val.influence = *inf;
-
-            // Remove the appropriate piece from the reserves.
-            let pieces_left_ref = &mut (if action >= 3 << 6 {
-                &mut self.update.caps_left
-            } else {
-                &mut self.update.stones_left
-            })[player];
-            *pieces_left_ref -= 1;
-            unmake.val.kind.place.pieces_left_ptr = (pieces_left_ref as *mut u8).addr();
-
-            let road_bit = (action as Bb >> 6 & 1) << sq;
-            let simd_road_bit = Simd::splat(road_bit);
-            let adjacent = *inf & simd_road_bit;
-
-            // Set the road and noble bits, if appropriate piece.
-            let new_road = self.copy.road ^ road_bit;
-            new.val.road = new_road;
-            new.val.noble = self.copy.noble ^ (action as Bb >> 7 & 1) << sq;
-
-            // Clear and set the owner.
-            let new_owner = self.copy.owner & !(1 << sq) | (player as Bb) << sq;
-            new.val.owner = new_owner;
-
-            // Placement is irreversible.
-            new.val.last_irreversible = self.copy.ply;
-
-            // Set the stack.
-            *unsafe { self.update.stacks.get_unchecked_mut(sq as usize) } = player as Stack | 2;
-
-            // Copy the tall bitboard unchanged.
-            new.val.tall = self.copy.tall;
-
-            // Check if the new road bit is adjacent to any of the influence masks.
-            if adjacent.reduce_or() != 0 {
-                // Prepare to add the neighbors to the adjacent masks.
-                let neighbors = adjacent
-                    .simd_ne(Simd::splat(0))
-                    .select(simd_road_bit.neighbors(), Simd::splat(0));
-
-                // Add the neighbors.
-                let mut old_inf = *inf;
-                *inf |= neighbors; // Codegens a spurious store. Alternatives worse.
-
-                // Get a bitboard representing our road pieces.
-                let my_road = Simd::splat(new_road & (new_owner ^ (player as Bb).wrapping_sub(1)));
-
-                // Expandable neighbors are those that are our road pieces.
-                let mut expandable = neighbors & my_road;
-
-                // Count iterations for logging purposes.
-                let mut i = 0;
-
-                // If we have any such neighbors that are not part of the old influence, expand.
-                // Assume winning placements are rare and/or handled before make.
-                while (expandable & !old_inf).reduce_or() != 0 {
-                    i += 1;
-                    old_inf = *inf;
-                    *inf |= expandable.neighbors();
-                    expandable = *inf & my_road;
-                }
-
-                log!(self, PlacementExpansionIterations(i));
-            }
-        }
-
-        self.update.hashes[new_ply] = hash;
     }
 }
